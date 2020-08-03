ad124bc24fba9683542ea5ae77d5fe223f51be09
spencerduncanpower <timdonerpower@gmail.com>
2020-08-03 17:22:42 -0400

    Add name to signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters 
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
   end
 end
